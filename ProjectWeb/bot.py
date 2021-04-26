cadb818f0e6aa08b0e6d169d6fd9abe50ceac26c
wula <krylovv455@gmail.com>
2021-04-26 20:20:04 +0300

    Bot telegram

--- ProjectWeb/bot.py
+++ ProjectWeb/bot.py
@@ -5,6 +5,7 @@ import json
 
 from pprint import pprint
 from validate_email import validate_email
+import transliterate
 
 from data.books import Book
 from data.users import User
@@ -52,6 +53,7 @@ def start_login(message):
             human = User()
             human.name = message.json['chat']['first_name']
             human.user_id = message.json['chat']['id']
+            human.subscription = 0
             db_sess.add(human)
             db_sess.commit()
             bot.send_message(message.from_user.id, answers[message.text][0])
@@ -74,8 +76,13 @@ def start_login(message):
 #         db_sess.commit()
 
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(content_types=['text', 'document'])
 def get_text_messages(message):
+    # markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    # markup.add('1', '2')  # Имена кнопок
+    # msg = bot.reply_to(message, 'Test text', reply_markup=markup)
+    # # bot.register_next_step_handler(msg, process_step)
+
     pprint(message.json)
     print()
     #Вход в личный кабинет и регистрация
@@ -87,7 +94,12 @@ def get_text_messages(message):
             find = True
         if not find:
             bot.send_message(message.from_user.id, 'Я тебя не знаю :(, напиши мне /login')
-    elif message.text == 'Книгу?':
+
+    elif message.text == 'Хочу загрузить книгу':
+        bot.send_message(message.from_user.id, 'Нам нужен файл:')
+        bot.register_next_step_handler(message, add_book)
+
+    elif message.text == 'Хочу скачать книгу':
         bot.send_message(message.from_user.id, 'Напиши название книги:')
         bot.register_next_step_handler(message, find_book)
 
@@ -96,46 +108,55 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, message.text)
 
 
-def find_book(message):
-    db_sess = db_session.create_session()
-    btn_title_books = []
-    for book in db_sess.query(Book).filter(Book.name.like(f"%{message.text}%")):
-        btn_title_books.append(types.InlineKeyboardButton(text='Нет', callback_data='no'))
-
-        # bot.send_document(message.from_user.id, open(book.container, 'r', encoding='utf-8'))
-
-
-def city(update,context):
-  list_of_cities = ['Erode','Coimbatore','London', 'Thunder Bay', 'California']
-  button_list = []
-  for each in list_of_cities:
-     button_list.append(InlineKeyboardButton(each, callback_data = each))
-  reply_markup=InlineKeyboardMarkup(build_menu(button_list,n_cols=1)) #n_cols = 1 is for single column and mutliple rows
-  bot.send_message(chat_id=update.message.chat_id, text='Choose from the following',reply_markup=reply_markup)
-
-
-def download_book(message):
+def add_book(message):
     file_info = bot.get_file(message.document.file_id)
     downloaded_file = bot.download_file(file_info.file_path)
 
-    src = 'files' + message.document.file_name
+    src = r'db\books\book' + message.document.file_name
     with open(src, 'wb') as new_file:
         new_file.write(downloaded_file)
 
-    bot.reply_to(message, "Пожалуй, я сохраню это")
+    bot.reply_to(message, "Осталось указать имя")
+    bot.register_next_step_handler(message, add_book_db, src)
 
-    bot.reply_to(message, "Пожалуй, я сохраню это")
+
+def add_book_db(message, src):
+    db_sess = db_session.create_session()
+    new_book = Book()
+    new_book.name = message.text
+    new_book.container = src
+    db_sess.add(new_book)
+    db_sess.commit()
+    bot.reply_to(message, "Пожалуй, я сохраню это!")
 
 
-def send_book(message):
-    if message.text == '1':
-        f = open("C:/Users/dmitl/Downloads/евгений онегин.fb2", "rb")
-        bot.send_document(message.chat.id, f)
-        bot.send_message(message.from_user.id, "приятного вам чтения")
-    if message.text == '2':
-        f = open("C:/Users/dmitl/Downloads/герой нашего времени.epub", "rb")
-        bot.send_document(message.chat.id, f)
-        bot.send_message(message.from_user.id, "приятного вам чтения")
+name = ''
+
+
+def find_book(message):
+    global name
+    name = message.text
+    db_sess = db_session.create_session()
+    btn_title_books = types.InlineKeyboardMarkup()
+
+    title_books = []
+    for k, book in enumerate(db_sess.query(Book).filter(Book.name.like(f"%{message.text}%"))):
+        btn_title_books.add(types.InlineKeyboardButton(text=book.name, callback_data=f'{k}'))
+        # title_books.append(book)
+
+    bot.send_message(message.from_user.id, text='Книга?', reply_markup=btn_title_books)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(message):
+    global name
+    db_sess = db_session.create_session()
+    title_books = []
+    for book in db_sess.query(Book).filter(Book.name.like(f"%{name}%")):
+        title_books.append(book)
+
+    bot.send_document(message.from_user.id, open(title_books[int(message.data)].container, 'r', encoding='utf-8'))
+    name = ''
 
 
 # bot.set_webhook()
