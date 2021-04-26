67e816587e276cb972a295543f0481a73e8bacba
wula <krylovv455@gmail.com>
2021-04-26 18:13:39 +0300

    Leon

--- ProjectWeb/bot.py
+++ ProjectWeb/bot.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 import apiai
 import json
 
@@ -40,9 +41,12 @@ def send_welcome(message):
 @bot.message_handler(commands=['login'])
 def start_login(message):
     if message.text == '/login':
+        flag = False
         db_sess = db_session.create_session()
 
-        if db_sess.query(User).filter(User.user_id == message.json['chat']['id']):
+        for i in db_sess.query(User).filter(User.user_id == message.json['chat']['id']):
+            flag = True
+        if flag:
             bot.send_message(message.from_user.id, 'А я тебя уже знаю <3')
         else:
             human = User()
@@ -83,7 +87,7 @@ def get_text_messages(message):
             find = True
         if not find:
             bot.send_message(message.from_user.id, 'Я тебя не знаю :(, напиши мне /login')
-    elif message.text == 'Можно книгу?':
+    elif message.text == 'Книгу?':
         bot.send_message(message.from_user.id, 'Напиши название книги:')
         bot.register_next_step_handler(message, find_book)
 
@@ -94,8 +98,44 @@ def get_text_messages(message):
 
 def find_book(message):
     db_sess = db_session.create_session()
+    btn_title_books = []
     for book in db_sess.query(Book).filter(Book.name.like(f"%{message.text}%")):
-        bot.send_document(message.from_user.id, open(book.container, 'r', encoding='utf-8'))
+        btn_title_books.append(types.InlineKeyboardButton(text='Нет', callback_data='no'))
+
+        # bot.send_document(message.from_user.id, open(book.container, 'r', encoding='utf-8'))
+
+
+def city(update,context):
+  list_of_cities = ['Erode','Coimbatore','London', 'Thunder Bay', 'California']
+  button_list = []
+  for each in list_of_cities:
+     button_list.append(InlineKeyboardButton(each, callback_data = each))
+  reply_markup=InlineKeyboardMarkup(build_menu(button_list,n_cols=1)) #n_cols = 1 is for single column and mutliple rows
+  bot.send_message(chat_id=update.message.chat_id, text='Choose from the following',reply_markup=reply_markup)
+
+
+def download_book(message):
+    file_info = bot.get_file(message.document.file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+
+    src = 'files' + message.document.file_name
+    with open(src, 'wb') as new_file:
+        new_file.write(downloaded_file)
+
+    bot.reply_to(message, "Пожалуй, я сохраню это")
+
+    bot.reply_to(message, "Пожалуй, я сохраню это")
+
+
+def send_book(message):
+    if message.text == '1':
+        f = open("C:/Users/dmitl/Downloads/евгений онегин.fb2", "rb")
+        bot.send_document(message.chat.id, f)
+        bot.send_message(message.from_user.id, "приятного вам чтения")
+    if message.text == '2':
+        f = open("C:/Users/dmitl/Downloads/герой нашего времени.epub", "rb")
+        bot.send_document(message.chat.id, f)
+        bot.send_message(message.from_user.id, "приятного вам чтения")
 
 
 # bot.set_webhook()
